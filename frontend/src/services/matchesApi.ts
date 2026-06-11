ee64a521211b857c8bcefe3c4d122539e83c3be2
Jose Manuel Garcia Lopez <jmgl.dev@gmail.com>
2026-06-11 12:51:47 +0200

    Fix register API URL

--- frontend/src/services/matchesApi.ts
+++ frontend/src/services/matchesApi.ts
@@ -1,4 +1,4 @@
-const API_URL = "import.meta.env.VITE_API_URL";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getWorldCupMatches = async (worldCupId: string, userId: string) => {
   const res = await fetch(`${API_URL}/worldcups/${worldCupId}/matches/${userId}`);
